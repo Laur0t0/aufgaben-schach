e4c6d165caa2f3ab3e48424bdc57aff6f421e92b
Laur0t0 <corvin2505@icloud.com>
2024-03-01 11:37:25 +0100

    01.03.24

--- knights/knightlogic.go
+++ knights/knightlogic.go
@@ -11,7 +11,31 @@ type BoardPos struct {
 // KnightAllowed erwartet ein Spielfeld und eine Position.
 // Liefert true, falls auf dieses Feld ein Springer gesetzt werden darf.
 func KnightAllowed(b board.Board, pos BoardPos) bool {
+
+	// Bedingungen:
+	// 	position(Zeile) > 0
+	// 	position(Zeile) < Länge der Liste(horizontal)
+	// 	position(Reihe) > 0
+	// 	position(Reihe) < Länge der Liste (vertikal)
+	// 	Position im Bord ist lehr
+
 	return pos.Row >= 0 && pos.Row < len(b) && pos.Col >= 0 && pos.Col < len(b[0]) && b[pos.Row][pos.Col] == " "
+
+	//oder mit if
+
+	// if pos.Row >= 0 && pos.Row < len(b) && pos.Col >= 0 && pos.Col < len(b[0]) && b[pos.Row][pos.Col] == " " {
+	// 	return true
+	// } else {
+	// 	return false
+	// }
+
+	//oder invertiert mit if
+
+	// if pos.Row < 0 || pos.Row >= len(b) || pos.Col < 0 || pos.Col >= len(b[0]) || b[pos.Row][pos.Col] != " " {
+	// 	return false
+	// }
+
+	// return true
 }
 
 // KnightNeighbours erwartet eine Spielfeldposition und liefert eine Liste mit
