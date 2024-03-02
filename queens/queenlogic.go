305205f43f6ce59370121a992e73f52f4a32061a
Laur0t0 <corvin2505@icloud.com>
2024-03-02 11:13:52 +0100

    Update queenlogic.go

--- queens/queenlogic.go
+++ queens/queenlogic.go
@@ -5,7 +5,7 @@ import "chesspuzzles/board"
 // QueenAllowed erwartet ein Spielfeld und eine Position.
 // Liefert true, falls es erlaubt ist, dort eine Dame zu setzen.
 func QueenAllowed(b board.Board, row, col int) bool {
-	// TODO
+	
 	return false
 }
 func HalloCorvin()bool{
